470fb8c50d9f3475f75c08d516b0d3dcf19bb7cd
switt_tv <4galarzajuan2004@gmail.com>
2023-09-20 22:20:37 -0600

    agregando funcionalidad para que la pagina se actualice al subirse nuevo video

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,12 +4,13 @@ export const load = (async () => {
     const res_video = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyC2ceG7RH57om1JYGxQopMfTJ1rs1nHsu8&q=svelterean&type=video&part=snippet`)
     let data_video = await res_video.json()
 
-    const res_comentario = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC2ceG7RH57om1JYGxQopMfTJ1rs1nHsu8&videoId=UoifAhH2qiA&part=snippet&order=relevance&maxResults=3`)
+    const res_comentario = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC2ceG7RH57om1JYGxQopMfTJ1rs1nHsu8&videoId=${data_video.items[0].id.videoId}&part=snippet&order=relevance&maxResults=3`)
     let data_comentario = await res_comentario.json()
 
     return {
         props: {
-            data: data_video, data2: data_comentario
+            data: data_video,
+            data2: data_comentario
         }
     };
 }) satisfies PageLoad;
